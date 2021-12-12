41a416ce71ac160a0e132c5aea65b2d61e7f8f2c
John Doe <john.doe@feedzai.com>
2021-12-12 00:43:44 +0000

    Fixed wrong path on documentation

--- documentation/src/components/subtitle/index.tsx
+++ documentation/src/components/subtitle/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
 
-export function Subtitle({ children }) {
+export const Subtitle: React.FC = ({ children }) => {
 	return <h2 className={styles.subtitle}>{children}</h2>;
 }
