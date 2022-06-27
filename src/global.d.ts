c1371ce94af0f0fcf8a96e174df9d22c21404e93
Joao Dias <joao.dias@feedzai.com>
2022-06-27 16:22:24 +0100

    fix: Add @testing-library/jest-dom to tsconfig

---
+++ src/global.d.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/*
+ * Please refer to the terms of the license
+ * agreement.
+ *
+ * (c) 2021 Feedzai, Rights Reserved.
+ */
+
+/**
+ * modules.d.ts
+ *
+ * @author João Dias <joao.dias@feedzai.com>
+ * @since 1.0.0
+ */
+declare module "*.css";
+
+declare interface Navigator extends NavigatorUA { }
+declare interface WorkerNavigator extends NavigatorUA { }
+
+// https://wicg.github.io/ua-client-hints/#navigatorua
+declare interface NavigatorUA {
+	readonly userAgentData?: NavigatorUAData;
+}
+
+// https://wicg.github.io/ua-client-hints/#dictdef-navigatoruabrandversion
+interface NavigatorUABrandVersion {
+	readonly brand: string;
+	readonly version: string;
+}
+
+// https://wicg.github.io/ua-client-hints/#dictdef-uadatavalues
+interface UADataValues {
+	readonly brands?: NavigatorUABrandVersion[];
+	readonly mobile?: boolean;
+	readonly platform?: string;
+	readonly architecture?: string;
+	readonly bitness?: string;
+	readonly model?: string;
+	readonly platformVersion?: string;
+	/** @deprecated in favour of fullVersionList */
+	readonly uaFullVersion?: string;
+	readonly fullVersionList?: NavigatorUABrandVersion[];
+	readonly wow64?: boolean;
+}
+
+// https://wicg.github.io/ua-client-hints/#dictdef-ualowentropyjson
+interface UALowEntropyJSON {
+	readonly brands: NavigatorUABrandVersion[];
+	readonly mobile: boolean;
+	readonly platform: string;
+}
+
+// https://wicg.github.io/ua-client-hints/#navigatoruadata
+interface NavigatorUAData extends UALowEntropyJSON {
+	getHighEntropyValues(hints: string[]): Promise<UADataValues>;
+	toJSON(): UALowEntropyJSON;
+}
