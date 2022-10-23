b889225fb4be5696a04721ad756b914526f6436f
Joao Dias <joao.dias@feedzai.com>
2022-10-23 16:47:53 +0100

    chore: Migrated to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,33 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import IstanbulPlugin from 'vite-plugin-istanbul';
-
+import dts from "vite-plugin-dts";
+import IstanbulPlugin from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
+		dts({
+			insertTypesEntry: true,
+		}),
 		IstanbulPlugin(),
 	],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, "src/index.tsx"),
+			name: "ReactA11yTools",
+			formats: ["es", "cjs"],
+			fileName: (format) => `index.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+				},
+			},
+		},
+	},
 });
