b9f61bbee86e975289c9263aac4ae7f3e4495ccb
João Dias <joao.dias@feedzai.com>
2024-02-20 00:48:26 +0000

    chore: migrated to vite 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import path from "node:path";
+/// <reference types="vitest" />
+/// <reference types="vitest/globals" />
+import { join, resolve } from "node:path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import IstanbulPlugin from "vite-plugin-istanbul";
 
@@ -9,13 +11,29 @@ export default defineConfig({
 	plugins: [
 		react(),
 		dts({
+			outDir: "dist/types",
 			insertTypesEntry: true,
 		}),
-		IstanbulPlugin(),
+		IstanbulPlugin({
+			cypress: true,
+			requireEnv: false,
+		}),
 	],
+	resolve: {
+		alias: [
+			{
+				find: /~(.+)/,
+				replacement: join(process.cwd(), "node_modules/$1"),
+			},
+			{
+				find: "src",
+				replacement: resolve(__dirname, "./src"),
+			},
+		],
+	},
 	build: {
 		lib: {
-			entry: path.resolve(__dirname, "src/index.tsx"),
+			entry: resolve(__dirname, "src/index.tsx"),
 			name: "ReactA11yTools",
 			formats: [
 				"es",
@@ -40,4 +58,42 @@ export default defineConfig({
 			},
 		},
 	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		include: ["./test/vitest/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		setupFiles: "./setupVitest.ts",
+		coverage: {
+			all: true,
+			enabled: true,
+			reporter: [
+				"html",
+				"json",
+				"text-summary"
+			],
+			provider: "istanbul",
+			reportsDirectory: './coverage-reports/vitest',
+			exclude: [
+				'coverage/**',
+				'dist/**',
+				'**/[.]**',
+				'packages/*/test?(s)/**',
+				'**/*.d.ts',
+				'**/virtual:*',
+				'**/__x00__*',
+				'**/\x00*',
+				'test/cypress/**',
+				'test?(s)/**',
+				'test?(-*).?(c|m)[jt]s?(x)',
+				'**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+				'**/__tests__/**',
+				'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+				'**/vitest.{workspace,projects}.[jt]s?(on)',
+				'**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}',
+				"documentation/**",
+				"coverage-reports/**",
+				"coverage/**"
+			],
+		},
+	}
 });
